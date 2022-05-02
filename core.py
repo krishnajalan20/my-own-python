d87935e4ba48de4c228417d9d38676fbe4acab35
krishnajalan20 <75469162+krishnajalan20@users.noreply.github.com>
2022-05-02 13:24:37 +0530

    Create core.py

---
+++ core.py
@@ -0,0 +1,8 @@
+#Bot Token From @BotFather
+bot_token = "5329536231:AAHvLBG39VtoOp3ahMN5pfSN5Psg68UPzIc"
+
+#Admins Telegram ID
+admins = [1773829449,5081665]
+
+#MongoDb Url For Databse
+mongo_url = "mongodb+srv://krishna1010:krishna1010@cluster0.vty4a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
