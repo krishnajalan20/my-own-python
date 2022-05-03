635a25eccef36fbcf9db4a5323c678e8fb5b215a
krishnajalan20 <75469162+krishnajalan20@users.noreply.github.com>
2022-05-03 11:12:56 +0530

    Update core.py

--- core.py
+++ core.py
@@ -1,7 +1,7 @@
 
 
 #Bot Token From @BotFather
-bot_token = "5130230092:AAFQcbnDqIrufFuOhp_BOoR48afJTiSjvig"
+bot_token = "5307170895:AAES7sY1lmpS1ovVl0wFcRa7WD61jHQqWDY"
 
 #Admins Telegram ID
 admins = [1773829449,5081665]
