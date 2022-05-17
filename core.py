5162242ea5bbb112fc01f651fe1df1ae7b98a293
krishnajalan20 <75469162+krishnajalan20@users.noreply.github.com>
2022-05-17 10:51:36 +0530

    Update core.py

--- core.py
+++ core.py
@@ -1,10 +1,10 @@
 
 
 #Bot Token From @BotFather
-bot_token = "5375012231:AAGNTrsEfbJvqR72Dzf5cjmMardD36nUxTY"
+bot_token = "5356539549:AAErCRhjUF5XkiJa4fM0Z36giW3RFTa_Wps"
 
 #Admins Telegram ID
 admins = [1773829449,5081665]
 
 #MongoDb Url For Databse
-mongo_url = "mongodb+srv://krishna1010:krishna1010@cluster0.vty4a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+mongo_url = "mongodb+srv://krishna101010:krishna101010@cluster0.vty4a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
